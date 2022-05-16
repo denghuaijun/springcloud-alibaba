a9efb0413f82f099373907b3725d4227530bf46c
denghuaijun <huaijundeng2020@163.com>
2022-05-16 16:39:39 +0800

    Initial commit

---
+++ src/main/java/com/denghuaijun/SpringcloudAlibabaApplication.java
@@ -0,0 +1,13 @@
+package com.denghuaijun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudAlibabaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudAlibabaApplication.class, args);
+    }
+
+}
